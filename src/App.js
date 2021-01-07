7159d6e46606b560ededd5807e761ad193b35738
Paulo <paulonova144@gmail.com>
2021-01-07 14:13:55 +0100

    before toggle dropdown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 
@@ -35,9 +35,15 @@ const options = [
 ];
 
 const App = () =>{
+
+  const [selected, setSelected] = useState(options[0]);
+
   return(
     <div>
-      <Dropdown options={options}/>
+      <Dropdown 
+      selected={selected} 
+      onSelectedChange={setSelected}
+      options={options}/>
     </div>
   );
 };
