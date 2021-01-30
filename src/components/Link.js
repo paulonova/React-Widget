ca0127aa99e0ce314d76939514002328ff4c6077
Paulo <paulonova144@gmail.com>
2021-01-30 13:13:37 +0100

    Project Finish

--- src/components/Link.js
+++ src/components/Link.js
@@ -4,8 +4,17 @@ import React from 'react';
 const Link = ({href, className, children})=>{
 
   const onClick = (event)=>{
+    if(event.metaKey || event.ctrlKey){
+      return;
+    }
+
     event.preventDefault();
-    window.history.pushState({}, '', )
+    window.history.pushState({}, '', href);
+
+    //Communicate to the rout that the url has changed..
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
+
   }
   return(
     <a onClick={onClick} href={href} className={className}>
